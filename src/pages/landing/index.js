b46b368ccbfcd5bb5558b7407f0e6863f3ac8af8
Henry Hamilton Holmes <hholmes726@gmail.com>
2021-02-23 16:15:02 -0500

    Installed react-router-dom, coded first route for the landing page, and created the greeting function in the landing page's index.js.

---
+++ src/pages/landing/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+
+var greeting = () => {
+    let today = new Date();
+    let hoursNow = today.getHours();
+
+    if (hoursNow > 18) {
+        return "Good evening!";
+    } else if (hoursNow > 12) {
+        return "Good afternoon!";
+    } else if (hoursNow > 0) {
+        return "Good morning!";
+    } else {
+        return "Greetings!"
+    }
+}
+
+const Landing = () => {
+    return (
+        <h1>{greeting()}</h1>
+    );
+};
+
+export default Landing;
