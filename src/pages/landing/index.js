6283d9341403fcf6337ecc29947c857e860d606c
Henry Hamilton Holmes <hholmes726@gmail.com>
2021-02-23 20:54:54 -0500

    Created navbar component, and wrote code to keep it hidden on the landing page.

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LandingContainer from '../../components/landing-container';
+import Navbar from '../../components/navbar';
 import Wrapper from '../../components/wrapper';
 
 import './style.css';
@@ -22,17 +23,20 @@ var greeting = () => {
 
 const Landing = () => {
     return (
-        <Wrapper>
-            <LandingContainer>
-                <h1>{greeting()}</h1>
-                <p>My name is <a className="anchor-link" href="https://www.linkedin.com/in/henryhamiltonholmes/">Hank</a>, and I'm a Web Developer based in <a className="anchor-link" href="https://en.wikipedia.org/wiki/Charlotte,_North_Carolina">Charlotte, NC</a>. I love designing, bulding, and implementing software solutions that create optimal user experiences and clearly convey the value of client goods and services.</p>
-                <div className="landing-page-links-container">
-                    <Link className="list-item-link" to="#">About</Link>
-                    <Link className="list-item-link" to="#">Work</Link>
-                    <Link className="list-item-link" to="#">Contact</Link>
-                </div>
-            </LandingContainer>
-        </Wrapper>
+        <>
+            <Navbar />
+            <Wrapper>
+                <LandingContainer>
+                    <h1>{greeting()}</h1>
+                    <p>My name is <a className="anchor-link" href="https://www.linkedin.com/in/henryhamiltonholmes/">Hank</a>, and I'm a Web Developer based in <a className="anchor-link" href="https://en.wikipedia.org/wiki/Charlotte,_North_Carolina">Charlotte, NC</a>. I love designing, bulding, and implementing software solutions that create optimal user experiences and clearly convey the value of client goods and services.</p>
+                    <div className="landing-page-links-container">
+                        <Link className="list-item-link" to="#">About</Link>
+                        <Link className="list-item-link" to="#">Work</Link>
+                        <Link className="list-item-link" to="#">Contact</Link>
+                    </div>
+                </LandingContainer>
+            </Wrapper>
+        </>
     );
 };
 
