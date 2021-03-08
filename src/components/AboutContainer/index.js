330fbcdd743b2223a8c60dd4400cb78ac00ad512
HHH-603 <hholmes726@gmail.com>
2021-03-08 11:31:14 -0500

    Created a AboutContainer component for the about page.

---
+++ src/components/AboutContainer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const AboutContainer = (props) => {
+    return (
+        <div className="about-container">{props.children}</div>
+    );
+};
+
+export default AboutContainer;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutContainer from '../../components/AboutContainer';
 import Footer from '../../components/footer';
 import Navbar from '../../components/navbar';
 import PageWrapper from '../../components/PageWrapper';
@@ -15,7 +16,7 @@ const About = () => {
                 <ProfileContainer>
                     <img className="profile" src={ProfilePic} alt="profile" />
                 </ProfileContainer>
-                <div className="about-container">
+                <AboutContainer>
                     <p>
                         I'm originally from <a className="about-link" href="https://en.wikipedia.org/wiki/Dunbarton,_New_Hampshire">Dunbarton, NH</a>, and recently earned my Full Stack Web Development certification from <a className="about-link" href="https://en.wikipedia.org/wiki/University_of_North_Carolina_at_Charlotte">UNC Charlotte</a> (class of 2021). I previously worked at <a className="about-link" href="https://www.everyaction.com/">EveryAction</a> in Washington, DC, where I helped equip mid-sized nonprofits with unified software solutions tailored to their fundraising and advocacy goals.
                     </p>
@@ -23,7 +24,7 @@ const About = () => {
                     <p>
                         When I'm not in front of my computer, I enjoy <a className="about-link" href="https://www.alltrails.com/us/north-carolina">hiking</a>, <a className="about-link" href="https://www.foodandwine.com/recipes">working on my cooking skills</a>, rooting for <a className="about-link" href="https://en.wikipedia.org/wiki/Boston_Celtics">the NBA's greatest franchise</a>, and exploring <a className="about-link" href="https://www.tripadvisor.com/Attractions-g49022-Activities-c36-t133-Charlotte_North_Carolina.html">the Queen City's outstanding craft breweries</a>.
                     </p>
-                </div>
+                </AboutContainer>
                 <div className="skills-container">
                     <h3>Some languages and technologies I've recently been working with are:</h3>
                     <ul className="skill-list">
