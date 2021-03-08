ec7e3ee25dfac076b792030fb6a0f46089ea43a6
HHH-603 <hholmes726@gmail.com>
2021-03-08 13:38:29 -0500

    Created a ProjectName component.

---
+++ src/components/ProjectName/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const ProjectName = (props) => {
+    return (
+        <h2 className="project-name">{props.children}</h2>
+    );
+};
+
+export default ProjectName;
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -12,6 +12,7 @@ import PageDescription from '../../components/PageDescription';
 import ProjectCard from '../../components/ProjectCard';
 import ImageContainer from '../../components/ImageContainer';
 import ProjectLinksContainer from '../../components/ProjectLinksContainer';
+import ProjectName from '../../components/ProjectName';
 
 const Work = () => {
     return (
@@ -29,9 +30,9 @@ const Work = () => {
                         <a className="project-image-link" href=""></a>
                         <img src={BT} className="project-image" alt="budget-tracker" />
                     </ImageContainer>
-                    <h2 className="project-name">Budget Tracker</h2>
-                    <p className="project-paragraph"><strong>Description: </strong>Web application that allows users to easily track their money (purchases and deposits), both online and offline.</p>
-                    <p className="project-paragraph"><strong>Languages/Technologies Used: </strong>JavaScript, HTML, Bootstrap, and MongoDB.</p>
+                    <ProjectName>Budget Tracker</ProjectName>
+                    <p><strong>Description: </strong>Web application that allows users to easily track their money (purchases and deposits), both online and offline.</p>
+                    <p><strong>Languages/Technologies Used: </strong>JavaScript, HTML, Bootstrap, and MongoDB.</p>
                     <ProjectLinksContainer>
                         <a className="project-link" href="https://quiet-temple-68742.herokuapp.com/">Deployed Application</a>
                         <a className="project-link" href="https://github.com/HHH-603/Online-Offline-Progressive-Web-App-Budget-Tracker">GitHub Repository</a>
@@ -42,9 +43,9 @@ const Work = () => {
                         <a className="project-image-link" href=""></a>
                         <img src={NT} className="project-image" alt="note-taker" />
                     </ImageContainer>
-                    <h2 className="project-name">Note Taker</h2>
-                    <p className="project-paragraph"><strong>Description: </strong>Web application where users can write, save, and delete notes.</p>
-                    <p className="project-paragraph"><strong>Languages/Technologies Used: </strong>JavaScript, Node.js, HTML, and Bootstrap.</p>
+                    <ProjectName>Note Taker</ProjectName>
+                    <p><strong>Description: </strong>Web application where users can write, save, and delete notes.</p>
+                    <p><strong>Languages/Technologies Used: </strong>JavaScript, Node.js, HTML, and Bootstrap.</p>
                     <ProjectLinksContainer>
                         <a className="project-link" href="https://hank-holmes-note-taker.herokuapp.com/">Deployed Application</a>
                         <a className="project-link" href="https://github.com/HHH-603/Note-Taker">Repository</a>
@@ -54,9 +55,9 @@ const Work = () => {
                     <ImageContainer>
                         <img src={RG} className="project-image" alt="readme-generator" />
                     </ImageContainer>
-                    <h2 className="project-name">README Generator</h2>
-                    <p className="project-paragraph"><strong>Description: </strong>Resource that generates a fully formatted README.md file based on user responses to questions that are displayed via their terminal.</p>
-                    <p className="project-paragraph"><strong>Languages/Technologies Used: </strong>JavaScript and Node.js.</p>
+                    <ProjectName>README Generator</ProjectName>
+                    <p><strong>Description: </strong>Resource that generates a fully formatted README.md file based on user responses to questions that are displayed via their terminal.</p>
+                    <p><strong>Languages/Technologies Used: </strong>JavaScript and Node.js.</p>
                     <ProjectLinksContainer>
                         <a className="project-link" href="https://github.com/HHH-603/Online-Offline-Progressive-Web-App-Budget-Tracker">
                             GitHub Repository
