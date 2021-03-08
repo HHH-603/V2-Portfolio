4543a69233d6fc74694b6c393c5ca6948d94a4c7
HHH-603 <hholmes726@gmail.com>
2021-03-08 13:59:25 -0500

    Added the contents of the contact page.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import HeaderContainer from '../../components/HeaderContainer';
 import Navbar from '../../components/navbar';
 import PageWrapper from '../../components/PageWrapper';
+import PageDescription from '../../components/PageDescription';
 import './style.css';
 
 const About = () => {
@@ -12,8 +13,14 @@ const About = () => {
             <Navbar />
             <HeaderContainer>
                 <Header>Contact</Header>
+                <PageDescription>I'm currently open to new opportunities, so please feel free to reach out!</PageDescription>
             </HeaderContainer>
             <PageWrapper>
+                <div className="contact-links-container">
+                    <a className="contact-link" href="mailto:hholmes726@gmail.com">Email</a>
+                    <a className="contact-link" href="https://www.linkedin.com/in/henryhamiltonholmes/">LinkedIn</a>
+                    <a className="contact-link" href="https://github.com/HHH-603">GitHub</a>
+                </div>
             </PageWrapper>
             <Footer />
         </>
