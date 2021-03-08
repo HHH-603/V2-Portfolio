a1c6dae5226c237a19d1cb8ccde01f9f2f47b5fe
HHH-603 <hholmes726@gmail.com>
2021-03-08 14:22:01 -0500

    Re-made the navbar as a flexbox.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,11 +6,10 @@ const Navbar = () => {
     return (
         <>
             <div className="navbar">
-                <h2 className="navbar-header">HHH</h2>
-                <Link className="navbar-link" to="/contact">Contact</Link>
-                <Link className="navbar-link" to="/work">Work</Link>
-                <Link className="navbar-link" to="/about">About</Link>
                 <Link className="navbar-link" to="/">Home</Link>
+                <Link className="navbar-link" to="/about">About</Link>
+                <Link className="navbar-link" to="/work">Work</Link>
+                <Link className="navbar-link" to="/contact">Contact</Link>
             </div>
         </>
     );
