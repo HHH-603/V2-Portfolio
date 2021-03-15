c9f0b6408e33641f6a2805484b66d562cbe974e9
HHH-603 <hholmes726@gmail.com>
2021-03-15 10:43:10 -0400

    Made formatting updates throughout my portfolio to enhance mobile responsiveness, and added a dropdown menu feature to my navigation component.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 const Navbar = () => {
+  const [sansToggle, withToggle] = useState(true);
+  const [menubar, tiltMenubar] = useState(true);
+
+  const handleToggle = () => {
+    withToggle(!sansToggle);
+  };
+
+  const handleTilt = () => {
+    tiltMenubar(!menubar);
+  };
+
   return (
     <>
       <div className="navbar">
+        <div className="menu-icon" onClick={handleToggle}>
+          <div
+            className="menu-bar"
+            // className={menubar ? "menu-bar" : "menu-bar-tilt"}
+            // onClick={handleTilt}
+          ></div>
+        </div>
+      </div>
+      <div className={sansToggle ? "dropdown-menu-hidden" : "dropdown-menu"}>
         <Link className="navbar-link" to="/">
           Home
         </Link>
